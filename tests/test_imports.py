bb9210d96324950e0c120ea308bf4e642c9b334a
eberrigan <berri104@gmail.com>
2025-02-19 10:05:21 -0800

    remove pythonpath manipulation

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -9,15 +9,3 @@ def test_import():
     from lablink_client import logging_utils
     from lablink_client import subscribe_instance
     from lablink_client import update_inuse_status
-
-
-if __name__ == "__main__":
-
-    # Add the parent directory to the pythonpath (pytest setup to do this automatically)
-    import os
-    import sys
-
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-    # Run the test
-    test_import()
