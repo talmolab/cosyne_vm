54f7fa7413b36e9ff5171602e25dffd241959529
7174Andy <hep003@ucsd.edu>
2025-02-20 11:32:39 -0800

    change to pytest

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,43 +1,44 @@
 from lablink_client.database import SpannerDatabase
-import unittest
+import pytest
 
-class TestSpannerDatabase(unittest.TestCase):
-    def test_create_database(self):
-        """
-        Test if the SpannerDatabase loads the existing database correctly
-        """
-        db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
-        self.assertIsNotNone(db)
+def test_create_database():
+    """
+    Test if the SpannerDatabase loads the existing database correctly
+    """
+    db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
+    assert db is not None
     
-    def test_get_column_names(self):
-        """
-        Test if the column names are fetched correctly
-        """
-        db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
-        actual_column_names = db.get_column_names()
-        expected_column_names = ['Hostname', 'Pin', 'CrdCmd', 'UserEmail', 'inUse']
-        self.assertEqual(actual_column_names, expected_column_names)
+def test_get_column_names():
+    """
+    Test if the column names are fetched correctly
+    """
+    db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
+    actual_column_names = db.get_column_names()
+    expected_column_names = ['Hostname', 'Pin', 'CrdCmd', 'UserEmail', 'inUse']
+    assert actual_column_names == expected_column_names
 
-    def test_read_data(self):
-        """
-        Test if the data is read correctly
-        """
-        db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
-        actual_data = db.read_data(table_name="Users")
-        expected_data = []
-        self.assertEqual(actual_data, expected_data)
+def test_read_data():
+    """
+    Test if the data is read correctly
+    """
+    db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
+    actual_data = db.read_data(table_name="Users")
+    expected_data = []
+    assert actual_data == expected_data
 
-    def test_vm_exists(self):
-        """
-        Test if the VM exists in the database
-        """
-        db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
-        actual_result = db.vm_exists(hostname="vm1")
-        self.assertFalse(actual_result)
+def test_vm_exists():
+    """
+    Test if the VM exists in the database
+    """
+    db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
+    actual_result = db.vm_exists(hostname="vm1")
+    expected_result = False
+    assert actual_result == expected_result
     
-    def test_vm_exists(self):
-        """
-        Test if the VM exists in the database
-        """
-        db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
-        self.assertRaises(ValueError, db.check_vm_exists, hostname="vm1")
+def test_check_vm_exists():
+    """
+    Test if the VM exists in the database
+    """
+    db = SpannerDatabase.load_database(project_id="vmassign-dev", instance_id="vmassign-test", database_id="users", table_name="Users")
+    with pytest.raises(ValueError):
+        db.check_vm_exists(hostname="vm1")
