20c6154339b151c651f93a14f21b4dba7e8036ba
7174Andy <hep003@ucsd.edu>
2025-02-25 10:21:26 -0800

    add destroy workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,6 @@
 locals {
   service_name  = "${var.resource_suffix}-cosyne-service"
   database_name = "users-${var.resource_suffix}"
-  is_test       = var.resource_suffix == "test"
-  is_staging    = var.resource_suffix == "staging"
-  is_production = var.resource_suffix == "prod"
-}
-
-# Service Account Creation for spanner instance
-resource "google_service_account" "spanner_admin_account" {
-  account_id   = "spanner-admin"
-  display_name = "Spanner Admin Service Account"
 }
 
 # Instance Creation
@@ -17,7 +8,7 @@ resource "google_spanner_instance" "database_instance" {
   name             = "vmassign-${var.resource_suffix}"
   display_name     = "Assign Instance ${var.resource_suffix}"
   config           = "regional-us-central1"
-  processing_units = 1000
+  processing_units = 10
 }
 
 
@@ -29,19 +20,4 @@ resource "google_spanner_database" "default" {
     "CREATE TABLE Users (Hostname STRING(1024) NOT NULL, Pin STRING(1024), CrdCmd STRING(1024), UserEmail STRING(1024), inUse BOOL,) PRIMARY KEY (Hostname)"
   ]
   deletion_protection = false
-}
-
-# Grant permissions to the new service account for Spanner
-resource "google_spanner_database_iam_member" "spanner_permissions" {
-  project  = var.project_id
-  instance = google_spanner_instance.database_instance.name
-  database = google_spanner_database.default.name
-  role     = "roles/spanner.databaseAdmin"
-  member   = "serviceAccount:${google_service_account.spanner_admin_account.email}"
-}
-
-# Service Account Creation for spanner instance
-resource "google_service_account" "cloud_run_admin" {
-  account_id   = "cloud-run-admin"
-  display_name = "Cloud Run Admin Service Account"
 }
